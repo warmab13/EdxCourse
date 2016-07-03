95995568ec817385ea7c92e2c309a70bcdd139fa
Alonso S <alonsosalcido13@gmail.com>
2016-07-03 17:52:38 -0600

    Trabajo sobre la MainActivity uso de los intents y de las singletone para manejar links e intents fuera del app

---
+++ app/src/main/java/edu/galileo/android/tipcalc/TipCalcApp.java
@@ -0,0 +1,14 @@
+package edu.galileo.android.tipcalc;
+
+import android.app.Application;
+
+/**
+ * Created by Warmab on 7/3/16.
+ */
+public class TipCalcApp extends Application {
+    private final static  String ABOUT_URL = "";
+
+    public static String getAboutUrl() {
+        return ABOUT_URL;
+    }
+}
